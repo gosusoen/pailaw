4ae696b9a57e4bcdc3e96d6727c2baf32c815b12
gosusoen@outlook.com <gosusoen@outlook.com>
2023-02-22 21:57:33 +0200

    22022023 progress 3

--- pailawcrawler/pailawcrawlerapp/finetune.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def combine_text_files(directory):
-    text = ''
-    for filename in os.listdir(directory):
-        if filename.endswith('.txt'):
-            with open(os.path.join(directory, filename), 'r') as f:
-                text += f.read() + '\n'
-    return text

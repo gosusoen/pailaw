4eda5878d39d3775aee65566b836138c640732c8
gosusoen@outlook.com <gosusoen@outlook.com>
2023-02-25 22:30:03 +0200

    25022023 progress 2

--- pailawapi/itteratorfunctiondemo.py
+++ pailawapi/itteratorfunctiondemo.py
@@ -1,16 +1,15 @@
 import requests
+from io import BytesIO
+from pdfminer.high_level import extract_text
 
 url = 'https://www.et.gr/api/DownloadFeksApi/?fek_pdf=20230100043'
 response = requests.get(url)
 
 if response.status_code == 200:
-    with open('sample.pdf', 'wb') as f:
-        f.write(response.content)
-    print('PDF file downloaded successfully.')
+    with BytesIO(response.content) as f:
+        text = extract_text(f)
+    print('PDF file downloaded and text extracted successfully.')
 else:
     print('Error:', response.status_code, response.text)
 
-# Open the file and read it in UTF-8 encoding
-with open('sample.pdf', 'r', encoding='utf-8') as f:
-    text = f.read()
-    print(text)
+print(text)

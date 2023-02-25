04b98ee5b7fe4dc21919324c390c4331137ca23b
gosusoen@outlook.com <gosusoen@outlook.com>
2023-02-26 00:59:12 +0200

    26022023 pdfcrawler 3

--- pailawapi/itteratorfunctiondemo.py
+++ pailawapi/itteratorfunctiondemo.py
@@ -25,21 +25,22 @@ def itteratefiles():
                 text = extract_text(f)
                 print(f'success {year}{calculatestring(category, 2)}{calculatestring(counter, 5)}')
         elif response.status_code == 500:
+            print('error 500 '+response.status_code)
             # for every N empty api calls, we change category
             emptyitterations+=emptyitterations
             if(emptyitterations > 100):
                 category += category
                 emptyitterations = 0
-                counter = 0
+                counter = 1
         else:
-           print('error'+response.status_code)
+           print('error other '+response.status_code)
 
         if counter < 99999:
             counter+=1
         else:
             counter=1
             if category < 20:
-                category+=1 
+                category+=1
             else:
                 year+=1
                 category=1
@@ -53,10 +54,10 @@ def calculatestring(number, maxdigits):
     output=""
     for digit in str(number):
        digits.append(int(digit))
-       for i in range(maxdigits - len(digits)):
-           output+="0"
+    for i in range(maxdigits - len(digits)):
+        output+="0"
     output +=str(number)
-    return output   
+    return output
    
 
 itteratefiles()

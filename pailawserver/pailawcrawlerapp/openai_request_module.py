bc7e9a11de59636fbccd6dd499f73ced75bc02cd
gosusoen@outlook.com <gosusoen@outlook.com>
2023-03-21 19:38:34 +0200

    21032023 pswap

--- pailawserver/pailawcrawlerapp/openai_request_module.py
+++ pailawserver/pailawcrawlerapp/openai_request_module.py
@@ -1,75 +1,31 @@
-import requests
-from io import BytesIO
-from pdfminer.high_level import extract_text
-import datetime
-import pytz
+from PyPDF2 import PdfReader
+import openai
 
-timezone = pytz.timezone('Europe/Athens')
-current_time = datetime.datetime.now(timezone)
-current_year = current_time.year
+# initializations
+openai.api_key = "***"
 
-class CounterClass:
-    def __init__(self):
-        self.year = 2023
-        self.category = 1
-        self.counter = 24
+model_engine = "text-davinci-002"
+temperature = 1
+max_tokens = 50
 
-obj = CounterClass()
+reader = PdfReader('examples_folder/f3.pdf')
 
-def itteratefiles():
-    calculate=True
-    emptyitterations = 0
-    text=""
-
-    while calculate:
-        url = f'https://www.et.gr/api/DownloadFeksApi/?fek_pdf={obj.year}{calculatestring(obj.category, 2)}{calculatestring(obj.counter, 5)}'
-        response = requests.get(url)
-        response.encoding = 'utf-8'
-        if response.status_code == 200:
-            with BytesIO(response.content) as f:
-                text = extract_text(f)
-                text = text.replace("\n", " ")
-                text = text.replace("- ", "")
-                # print(f'success {obj.year}{calculatestring(obj.category, 2)}{calculatestring(obj.counter, 5)}\n')
-                print(text)
-        elif response.status_code == 500:
-            print('error 500 '+ response.status_code)
-            # for every N empty api calls, we change category
-            emptyitterations+=1
-            if(emptyitterations > 4):
-                emptyitterations = 0
-                increment(obj)
-        else:
-            print('error other '+ response.status_code)
-        calculate = increment(obj)
-        # print(f'{obj.year}{calculatestring(obj.category, 2)}{calculatestring(obj.counter, 5)}\n')
-
-def increment(obj):
-    result = True
-    if obj.counter < 99:
-            obj.counter+=1
-    else:
-        obj.counter=1
-        if obj.category < 20:
-            obj.category+=1
-        else:
-            obj.year+=1
-            obj.category=1
-            obj.counter=1          
-            if obj.year > current_year:
-                result = False
-    return result
+# text calculation
+text = ''
+for i in range(len(reader.pages)):
+    page = reader.pages[i]
+    text = page.extract_text()
+    print(i)
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt="Θέλω να μου απαντήσεις" + text,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
 
 
-def calculatestring(number, maxdigits):
-    digits = []
-    output=""
-    for digit in str(number):
-       digits.append(int(digit))
-    for i in range(maxdigits - len(digits)):
-        output+="0"
-    output +=str(number)
-    return output
-   
 
-itteratefiles()
